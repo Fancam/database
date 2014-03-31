f00fb54a338568ffba946fe6f040bad433ae2431
Felix Kiss <felix@felixkiss.com>
2014-03-31 16:40:22 +0200

    ♻️ Refactor database spec

--- spec/Felixkiss/Database/DatabaseSpec.php
+++ spec/Felixkiss/Database/DatabaseSpec.php
@@ -50,36 +50,34 @@ class DatabaseSpec extends ObjectBehavior
              ->shouldReturnAnInstanceOf('PDOStatement');
     }
 
-    function its_select_should_use_numbered_parameters_if_given($pdo, PDOStatement $statement)
+    function its_select_should_use_numbered_parameters_if_given($pdo)
     {
-        $pdo->prepare('SELECT * FROM foo WHERE bar = ?')
-            ->willReturn($statement);
+        $statement = $this->mockStatementFor($pdo, 'SELECT * FROM foo WHERE bar = ?');
+
         $statement->bindValue(1, 'baz', PDO::PARAM_STR)
-                  ->shouldBeCalled();
-        $statement->execute()
                   ->shouldBeCalled();
 
         // when
         $this->select('SELECT * FROM foo WHERE bar = ?', ['baz']);
     }
 
-    function its_select_should_use_named_parameters_if_given($pdo, PDOStatement $statement)
+    function its_select_should_use_named_parameters_if_given($pdo)
     {
-        $pdo->prepare('SELECT * FROM foo WHERE bar = :bar')
-            ->willReturn($statement);
+        $statement = $this->mockStatementFor($pdo, 'SELECT * FROM foo WHERE bar = :bar');
+
         $statement->bindValue(':bar', 'baz', PDO::PARAM_STR)
-                  ->shouldBeCalled();
-        $statement->execute()
                   ->shouldBeCalled();
 
         // when
         $this->select('SELECT * FROM foo WHERE bar = :bar', [':bar' => 'baz']);
     }
 
-    function its_select_should_use_correct_value_type_if_binding_parameters($pdo, PDOStatement $statement)
+    function its_select_should_use_correct_value_type_if_binding_parameters($pdo)
     {
-        $pdo->prepare('SELECT * FROM foo WHERE int_field = ? AND bool_field = ? AND null_field IS ? AND other_field = ?')
-            ->willReturn($statement);
+        $statement = $this->mockStatementFor($pdo,
+            'SELECT * FROM foo WHERE int_field = ? AND bool_field = ? AND null_field IS ? AND other_field = ?'
+        );
+
         $statement->bindValue(1, 123, PDO::PARAM_INT)
                   ->shouldBeCalled();
         $statement->bindValue(2, true, PDO::PARAM_BOOL)
@@ -87,8 +85,6 @@ class DatabaseSpec extends ObjectBehavior
         $statement->bindValue(3, null, PDO::PARAM_NULL)
                   ->shouldBeCalled();
         $statement->bindValue(4, 'baz', PDO::PARAM_STR)
-                  ->shouldBeCalled();
-        $statement->execute()
                   ->shouldBeCalled();
 
         // when
@@ -100,21 +96,37 @@ class DatabaseSpec extends ObjectBehavior
         ]);
     }
 
-    function it_should_insert_records_using_prepared_statements($pdo, PDOStatement $statement)
+    function it_should_insert_records_using_prepared_statements($pdo)
     {
-        $pdo->prepare('INSERT INTO users (username, city) VALUES (:username, :city)')
-            ->shouldBeCalled()
-            ->willReturn($statement);
-        $statement->bindValue(Argument::cetera())
+        $statement = $this->mockStatementFor($pdo, 'INSERT INTO users (username, city) VALUES (:username, :city)');
+
+        $statement->bindValue(':username', Argument::cetera())
                   ->shouldBeCalled();
-        $statement->execute()
-                  ->shouldBeCalled();
-        $statement->closeCursor()
+        $statement->bindValue(':city', Argument::cetera())
                   ->shouldBeCalled();
 
+        // when
         $this->insert('users', [
             'username' => 'felixkiss',
             'city' => 'Vienna, Austria',
         ]);
+    }
+
+    private function mockStatementFor($pdo, $query)
+    {
+        $prophet = new \Prophecy\Prophet;
+
+        $statement = $prophet->prophesize();
+        $statement->willExtend('PDOStatement');
+        $statement->execute()
+                  ->willReturn(null);
+        $statement->closeCursor()
+                  ->willReturn(null);
+
+        $pdo->prepare($query)
+            ->shouldBeCalled()
+            ->willReturn($statement);
+
+        return $statement;
     }
 }
